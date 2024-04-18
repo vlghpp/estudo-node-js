9e9d81ca8b6cc3df3d504e4107c61f056dbea637
Henrique Patricio <henriquepworking@gmail.com>
2024-04-18 20:22:45 -0300

    Colocando arquivos .js só para aparecer a pasta no github, o conteúdo deste dia pode ser encontrado no readme.md

---
+++ Dia 06 - Conhecendo APIs HTTP/intex.js
@@ -0,0 +1,2 @@
+//Este arquivo foi criado só para aparecer no github (se n tiver nada dentro da pasta ele n sobe a pasta)
+//Todo conteúdo deste dia pode ser encontrado na documentação (readme.md) na raiz.
